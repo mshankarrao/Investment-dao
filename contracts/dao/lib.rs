58b71dc78c994615197a9812f9f66d8f8d4f9b6c
Shankar Rao Mata <mshankarrao1970@gmail.com>
2023-08-04 20:32:23 -0400

    completed the code for both dao and governance token

--- contracts/dao/lib.rs
+++ contracts/dao/lib.rs
@@ -2,6 +2,8 @@
 
 #[ink::contract]
 pub mod dao {
+
+    use crate::ensure;
     use ink::storage::Mapping;
     use openbrush::contracts::traits::psp22::*;
     use scale::{
@@ -9,16 +11,28 @@ pub mod dao {
         Encode,
     };
 
+    type ProposalId = u32;
+
     #[derive(Encode, Decode)]
     #[cfg_attr(feature = "std", derive(Debug, PartialEq, Eq, scale_info::TypeInfo))]
     pub enum VoteType {
         // to implement
+        Against,
+        For,
     }
 
     #[derive(Copy, Clone, Debug, PartialEq, Eq, Encode, Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum GovernorError {
-        // to implement
+        AmountShouldNotBeZero,
+        DurationError,
+        ProposalNotFound,
+        VotePeriodEnded,
+        ProposalAlreadyExecuted,
+        AlreadyVoted,
+        QuorumNotReached,
+        ProposalNotAccepted
+
     }
 
     #[derive(Encode, Decode)]
@@ -32,8 +46,13 @@ pub mod dao {
             ink::storage::traits::StorageLayout
         )
     )]
+
     pub struct Proposal {
-        // to implement
+        to: AccountId,
+        vote_start: u64,
+        vote_end: u64,
+        executed: bool,
+        amount: Balance,
     }
 
     #[derive(Encode, Decode, Default)]
@@ -49,17 +68,41 @@ pub mod dao {
     )]
     pub struct ProposalVote {
         // to implement
+        for_votes: u64,
+        against_vote: u64,
     }
 
     #[ink(storage)]
     pub struct Governor {
-        // to implement
+        governance_token: AccountId,
+        quorum: u8,
+        proposals: Mapping<ProposalId,Proposal>,
+        proposal_votes: Mapping<ProposalId,ProposalVote>,
+        votes: Mapping<(ProposalId,AccountId),()>,
+        next_proposal_id: ProposalId,
     }
 
     impl Governor {
         #[ink(constructor, payable)]
         pub fn new(governance_token: AccountId, quorum: u8) -> Self {
-            unimplemented!()
+            Self {
+                governance_token,
+                quorum,
+                proposals: Mapping::new(),
+                proposal_votes:  Mapping::new(),
+                votes: Mapping::new(),
+                next_proposal_id: 0,
+            }
+        }
+
+        #[ink(message)]
+        pub fn next_proposal_id(&mut self) -> ProposalId{
+            self.next_proposal_id + 1
+        }
+
+        #[ink(message)]
+        pub fn get_proposal(&mut self, proposal_id: ProposalId) -> Result<Proposal,GovernorError>{
+            Ok(self.proposals.get(proposal_id).unwrap())
         }
 
         #[ink(message)]
@@ -69,7 +112,18 @@ pub mod dao {
             amount: Balance,
             duration: u64,
         ) -> Result<(), GovernorError> {
-            unimplemented!()
+            ensure!(amount == 0, GovernorError::AmountShouldNotBeZero);
+            ensure!(duration == 0, GovernorError::DurationError);
+            let proposal = Proposal{
+                to,
+                vote_start: self.env().block_timestamp(),
+                vote_end: duration,
+                executed: false,
+                amount,
+            };
+            self.proposals.insert( self.next_proposal_id, &proposal);
+            Ok(())
+    
         }
 
         #[ink(message)]
@@ -78,12 +132,45 @@ pub mod dao {
             proposal_id: ProposalId,
             vote: VoteType,
         ) -> Result<(), GovernorError> {
-            unimplemented!()
+            ensure!(self.proposals.get(proposal_id).is_none(),GovernorError::ProposalNotFound);
+            ensure!(self.proposals.get(proposal_id).unwrap().executed == true,GovernorError::ProposalAlreadyExecuted);
+            ensure!(self.proposals.get(proposal_id).unwrap().vote_end > self.env().block_timestamp(),GovernorError::VotePeriodEnded);
+            ensure!(self.votes.get((proposal_id,self.env().caller())).is_none(),GovernorError::AlreadyVoted);
+            self.votes.insert((proposal_id, self.env().caller()),&());
+            let mut weight = self.env().balance();
+            let total_supply = ink::env::call::build_call:: <ink::env::DefaultEnvironment>()
+            .call(self.governance_token)
+            .gas_limit(5_000_000_000)
+            .exec_input(
+                ink::env::call::ExecutionInput::new(ink::env::call::Selector::new(ink::selector_bytes!("PSP22::total_supply")))
+            )
+            .returns::<Balance>()
+            .try_invoke(); 
+            weight = weight/total_supply.unwrap().unwrap();
+        
+         match vote{
+            VoteType::For => self.proposal_votes.get(proposal_id).unwrap().for_votes as u128 + weight,
+            VoteType::Against => self.proposal_votes.get(proposal_id).unwrap().against_vote as u128 + weight,
+         };
+
+            Ok(())
+            
         }
 
         #[ink(message)]
         pub fn execute(&mut self, proposal_id: ProposalId) -> Result<(), GovernorError> {
-            unimplemented!()
+            ensure!(self.proposals.get(proposal_id).is_none(),GovernorError::ProposalNotFound);
+            ensure!(self.proposals.get(proposal_id).unwrap().executed == true,GovernorError::ProposalAlreadyExecuted);
+            let total_votes = (self.proposal_votes.get(proposal_id).unwrap().for_votes + self.proposal_votes.get(proposal_id).unwrap().against_vote) as u8;
+            if total_votes < self.quorum {
+                return Err(GovernorError::QuorumNotReached)
+            }
+            ensure!(self.proposal_votes.get(proposal_id).unwrap().for_votes >= 50, GovernorError::ProposalNotAccepted);
+            ensure!(self.votes.get((proposal_id,self.env().caller())).is_none(),GovernorError::AlreadyVoted);
+            self.proposals.get(proposal_id).unwrap().executed = true;
+           // self.proposals.get(proposal_id).unwrap().to.transfer(self.proposals.get(proposal_id).unwrap().amount);
+
+            Ok(())
         }
 
         // used for test
@@ -97,8 +184,10 @@ pub mod dao {
     mod tests {
         use super::*;
 
+        const ONE_MINUTE:u64 = 60;
+
         fn create_contract(initial_balance: Balance) -> Governor {
-            let accounts = default_accounts();
+            let accounts: ink::env::test::DefaultAccounts<ink::env::DefaultEnvironment> = default_accounts();
             set_sender(accounts.alice);
             set_balance(contract_id(), initial_balance);
             Governor::new(AccountId::from([0x01; 32]), 50)
@@ -156,9 +245,20 @@ pub mod dao {
         fn quorum_not_reached() {
             let mut governor = create_contract(1000);
             let result = governor.propose(AccountId::from([0x02; 32]), 100, 1);
+            let voting = governor.vote(0, VoteType::Against);
             assert_eq!(result, Ok(()));
             let execute = governor.execute(0);
             assert_eq!(execute, Err(GovernorError::QuorumNotReached));
         }
     }
+}
+
+
+#[macro_export]
+macro_rules! ensure {
+    ( $x:expr, $y:expr $(,)? ) => {{
+        if $x {
+            return Err($y.into());
+        }
+    }};
 }
--- contracts/governance-token/lib.rs
+++ contracts/governance-token/lib.rs
@@ -1,1 +1,32 @@
-// Implement PSP2 + PSP22Metadata
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[openbrush::implementation(PSP22, PSP22Metadata)]
+#[openbrush::contract]
+pub mod governance_token {
+    use openbrush::traits::Storage;
+
+    #[ink(storage)]
+    #[derive(Default, Storage)]
+    pub struct GovernanceToken {
+        #[storage_field]
+        psp22: psp22::Data,
+        #[storage_field]
+        metadata: metadata::Data,
+    }
+
+    impl GovernanceToken {
+        #[ink(constructor)]
+        pub fn new(total_supply: Balance, name: Option<String>, symbol: Option<String>, decimal: u8) -> Self {
+            let mut instance = Self::default();
+            let caller = instance.env().caller();
+
+            instance.metadata.name.set(&name);
+            instance.metadata.symbol.set(&symbol);
+            instance.metadata.decimals.set(&decimal);
+
+            psp22::Internal::_mint_to(&mut instance, caller, total_supply).expect("Should mint total_supply");
+
+            instance
+        }
+    }
+}
